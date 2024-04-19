cd2fb2561ed1aafc0865ff11fd7111e5ae3f7147
moc-lt-844 <neeraj.sharma@werize.com>
2024-04-19 12:41:41 +0530

    dummy project setup

---
+++ dummy/src/test/java/com/project/dummy/DummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
